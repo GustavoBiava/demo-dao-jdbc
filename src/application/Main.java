855ac00e26cc8a384f3af802002c1df049ebcf75
Gustavo Biava <gustavo.rbiava@hotmail.com>
2024-01-18 18:24:30 -0300

    Seller entity class

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
 
         Department department = new Department(1,"Books");
-        System.out.println(department);
+
+        Seller seller = new Seller(14,"Cristiano", "cristiano@gmail.com", LocalDate.now(),5000.00, department);
+        System.out.println(seller);
 
     }
 }
